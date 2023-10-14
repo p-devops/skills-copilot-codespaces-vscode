e63ade796949f7959acb9787e615a5d064826f55
peter leung <49036546+p-devops@users.noreply.github.com>
2023-10-14 15:07:17 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember () {
+  return {
+    restrict: 'E',
+    templateUrl: 'views/skills-member.html',
+    controller: 'SkillsMemberController',
+    controllerAs: 'skillsMemberCtrl'
+  };
+}
